ae63ecdc0c5eb20fa2cc9e1d2f1a201c0b9c0652
isaki-na <isara.esc.5@gmail.com>
2025-01-30 19:46:16 -0600

    cosacosa

--- Practica.php
+++ Practica.php
@@ -8,5 +8,15 @@ class Persona {
     $this->nombre = $nombre;
     $this->edad = $edad;
     }
+    private function definirNombre(){
+        return $this->nombre;
+    }
+    public function saludar(){
+        echo "<p>".$this->definirNombre()."</p>";
+    }
 }
+$persona3 = new Persona("Awawawa",20);
+$persona3->saludar();
+$persona2 = new Persona("Awawo",21);
+$persona2->saludar();
 ?>
