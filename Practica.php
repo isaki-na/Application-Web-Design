29fad822806170ab67735dd2caa5ac1df5262b83
isaki-na <isara.esc.5@gmail.com>
2025-01-30 19:53:21 -0600

    444

--- Practica.php
+++ Practica.php
@@ -19,4 +19,18 @@ $persona3 = new Persona("Awawawa",20);
 $persona3->saludar();
 $persona2 = new Persona("Awawo",21);
 $persona2->saludar();
+
+class animal {
+    public function saludar(){
+        return "Hola soy un animal";
+    }
+}
+class perro extends animal{
+    public function hacerSonido(){
+        return "Guaf guaf guaf guaf";
+    }
+}
+$perrito = new perro();
+echo "<p>".$perrito->saludar() ."</p>";
+echo "<p>".$perrito->hacerSonido() ."</p>";
 ?>
