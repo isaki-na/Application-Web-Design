a55a632b539237d92c3b8e1bce97e7c7db20d380
isaki-na <isara.esc.5@gmail.com>
2025-01-30 19:39:47 -0600

    Practica cambios

---
+++ Practica.php
@@ -0,0 +1,12 @@
+<?php
+class Persona { 
+    private $nombre;
+    private $edad;
+
+    public function __construct($nombre, $edad) {
+
+    $this->nombre = $nombre;
+    $this->edad = $edad;
+    }
+}
+?>
